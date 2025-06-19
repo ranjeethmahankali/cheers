1080d78312b95ce7c8a220eeb506d953b2f6d875
Ranjeeth Mahankali <ranjeethmahankali@gmail.com>
2025-06-19 00:06:04 -0400

    Start over without egui

--- src/main.rs
+++ src/main.rs
@@ -1,40 +1,25 @@
-use egui::{Color32, Pos2, Rect, Style};
+/*
+The hexagonal grids are stored in a coordinate system where the axes are
+squished together to 60 degrees.
 
-fn main() -> Result<(), Error> {
-    eframe::run_native(
-        "Cheers",
-        eframe::NativeOptions {
-            viewport: egui::ViewportBuilder::default()
-                .with_maximized(true)
-                .with_clamp_size_to_monitor_size(true),
-            ..Default::default()
-        },
-        Box::new(|_cc| Ok(Box::from(VisApp::default()))),
-    )
-    .map_err(Error::GuiFailure)
-}
+           (0, 1) * ------- * (1, 1)
+                 / \       / \
+                /   \     /   \
+               /     \   /     \
+              /       \ /       \
+      (0, 0) * ------- * ------- * (2, 0)
+            / \     (1, 0)      /
+           /   \     /   \     /
+          /     \   /     \   /
+         /       \ /       \ /
+        * ------- * ------- *
+    (0, -1)     (1, -1)     (2, -1)
 
-#[derive(Debug)]
-enum Error {
-    GuiFailure(eframe::Error),
-}
+This is because a hexagonal grid and a rectangular grid are topologically
+equivalent. The only difference is that, in the convention of the above diagram,
+the diagonal in the (-1, 1) direction and the diagonal (1, -1) are considered
+neighbors of the point in the middle. This means I can store the points in a
+rectangular grid, and infer the connectivity from the indices.
+ */
 
-#[derive(Default)]
-struct VisApp {}
-
-impl eframe::App for VisApp {
-    fn update(&mut self, ctx: &egui::Context, frame: &mut eframe::Frame) {
-        let style = Style::default();
-        egui::CentralPanel::default().show(ctx, |ui| {
-            let mut rect = ui.available_rect_before_wrap();
-            egui::Scene::new().show(ui, &mut rect, |ui| {
-                let painter = ui.painter();
-                painter.rect_filled(
-                    Rect::from_min_max(Pos2 { x: 10., y: 10. }, Pos2 { x: 100., y: 100. }),
-                    0.1,
-                    Color32::from_rgb(200, 50, 50),
-                );
-            })
-        });
-    }
-}
+fn main() {}
